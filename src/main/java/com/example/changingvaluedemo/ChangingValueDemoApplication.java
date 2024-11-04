6821cc53dd647ed046f534dab86bfce0797494b4
Yevhen Bufan <partner.Yevhen.Bufan@philips.com>
2024-11-04 19:21:31 +0200

    testbranchtestbranchtestbranchtestbranch

--- src/main/java/com/example/changingvaluedemo/ChangingValueDemoApplication.java
+++ src/main/java/com/example/changingvaluedemo/ChangingValueDemoApplication.java
@@ -9,6 +9,9 @@ public class ChangingValueDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChangingValueDemoApplication.class, args);
         System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
     }
 
 }
